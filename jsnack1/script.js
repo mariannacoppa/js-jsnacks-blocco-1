991667a9251d176bc6795024185a935c3cd4b21f
mariannacoppa <densimetra@gmail.com>
2024-07-01 16:10:32 +0200

    [UPD] set array elements

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,6 @@
 // SNACK 1: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+// definisco elenco degli invitati
+const invitati = ["Jay", "Daisy", "Nick", "Patrick", "Scott", "Francis", "Susan", "Edward", "Stuart"];
+console.log(invitati);
