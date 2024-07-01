eade28d9bb16ef5d33671b7b248db771badd61c4
mariannacoppa <densimetra@gmail.com>
2024-07-01 17:00:45 +0200

    [FIX] finish jsnack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,30 +1,30 @@
 // SNACK 1: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-// definisco elenco degli invitati
-const invitati = ["Jay", "Daisy", "Nick", "Patrick", "Scott", "Francis", "Susan", "Edward", "Stuart"];
+// definisco elenco di invitati
+const invitati = ["Jay", "Daisy", "Nick", "Susan", "Ann", "Liz", "Patrick", "Thomas", "Robert"];
 console.log(invitati);
-// recupero il pulsante dal DOM
+// recupero il pulsante dal dom
 const button = document.getElementById("submit");
-// aggiungo evento al tag button
+
 button.addEventListener('click', function () {
     // recupero il valore inserito nel campo input
-    let guest = document.getElementById('name').ariaValueMax.toLowerCase();
-    console.log(guest);
-    // inserisco variabile di controllo flag
+    let firstname = document.getElementById('firstname').value.toLowerCase();
+    console.log(firstname);
+    // inserisco la variabile di controllo (flag)
     let found_invitati = false;
-    // ciclo l'array e confronto l'elemento da cercare con la mail inserita nel form
-    for (let i = 0; i < invitati.lenght; i++) {
-        if (invitati[i].toLowerCase() === guest) {
+    // ciclo l'array e confronto l'elemento da cercare con il nome inserito nel form
+    for (let i = 0; i < invitati.length; i++) {
+        if(invitati[i].toLowerCase() === firstname) {
             // se la condizione è vera, imposto la variabile di controllo true
             found_invitati = true;
         }
     }
     // creo messaggio con contenuto in base al valore della variabile di controllo
     if (found_invitati === true) {
-        alert(`Dear ${guest.toLowerCase()}, you are welcome to the Party`);
+        alert(`Dear ${firstname.toUpperCase()}, you are welcome to the Party`);
     }
     else {
-        alert(`Sorry ${guest.toLowerCase()}, your entry is not granted`);
+        alert(`Sorry ${firstname.toUpperCase()}, your entry is not allowed`);
     }
 });
