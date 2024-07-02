8a551a83c924d1ebd69a939c3eb186307ff1fe1e
mariannacoppa <densimetra@gmail.com>
2024-07-02 15:13:25 +0200

    [UPD] let user enter a number

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,5 +3,6 @@
 let numbers = [];
 // lancio un ciclo di 6 iterazioni
 for (let i = 0; i <= 6; i++) {
-    
+    // permetto l'inserimento del numero da parte dell'utente
+    let num = prompt('inserisci un numero');
 }
