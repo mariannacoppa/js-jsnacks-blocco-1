36c8d45d6cf87a8b1742f14adffa70d69d9868b1
mariannacoppa <densimetra@gmail.com>
2024-07-02 15:26:02 +0200

    [UPD] get user value from tag

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,5 +1,6 @@
 // SNACK 3:
 // permetto all'utente di inserire un numero
 document.getElementById('number').addEventListener('click', function () {
-    
+    // recupero il valore dal campo input inserito dall'utente
+    let num = document.getElementById('number').value;
 })
