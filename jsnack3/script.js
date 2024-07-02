0b9c757e02bb9325b4ab2cf071e49a3a187c8cc5
mariannacoppa <densimetra@gmail.com>
2024-07-02 15:39:48 +0200

    [UPD] set variable that shows sum cipher

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,6 +5,14 @@ document.getElementById('number').addEventListener('click', function () {
     let num = document.getElementById('number').value;
     // verifico che il valore inserito sia lungo esattamente 4 cifre
     if (num.length === 4) {
-        
+        // dichiaro la variabile che contiene la somma delle cifre
+        let somma = 0;
+        // ciclo i valori inseriti
+        for(let i = 0; i < num.length; i++) {
+            // dichiaro e inizializzo la variabile con il primo numero intero
+            somma += parseInt(num[i]);
+        }
+        // mostro la variabile
+        console.log(somma);
     }
 })
