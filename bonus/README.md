65cd09fcf89351414b6333cf5b75406d0e0f771c
mariannacoppa <densimetra@gmail.com>
2024-07-02 16:03:00 +0200

    [UPD] written js code FINE BONUS

--- bonus/README.md
+++ bonus/README.md
@@ -7,4 +7,4 @@ Pseudocodice
 1 dichiaro e inizializzo la variabile somma
 2 definisco le variabili inserite dall'utente e le ciclo
 2.1 definisco la variabile somma
-3 mostro il valoe della variabile somma
+3 mostro il valore della variabile somma
---
+++ bonus/script.js
@@ -0,0 +1,10 @@
+// dichiaro e inizializzo la variabile somma
+let somma = 0;
+// definisco le variabili inserite dall'utente e le ciclo
+for (let i = 0; i < 10; i++) {
+    let num = parseInt(prompt(`inserisci il ${i + 1}° numero`));
+    // definisco la variabile somma
+    somma +- num;
+};
+// mostro il valore della variabile somma
+console.log(somma);
