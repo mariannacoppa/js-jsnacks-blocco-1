da6c66e115ef6c8792cc5203ef828251d9deee8e
mariannacoppa <densimetra@gmail.com>
2024-07-02 15:16:31 +0200

    [UPD] selected odd numbers and put them into the array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,4 +5,9 @@ let numbers = [];
 for (let i = 0; i <= 6; i++) {
     // permetto l'inserimento del numero da parte dell'utente
     let num = prompt('inserisci un numero');
+    // verifico: se il numero è dispari
+    if (num % 2 != 0) {
+        // allora inserisco il numero nell'array
+        numbers.push(num);
+    }
 }
